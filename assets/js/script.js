38db298ffa5e7ffa9c467eb554da8b76a3d00188
Jacob Corum <cor15031@byui.edu>
2020-06-06 14:04:23 -0600

    fixed api call

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,15 @@
 var apiKey = "a2dfa14dde9a15c432f7f0a5cb10b4a7";
 var historyList = JSON.parse(localStorage.getItem("historyList"));
 
-
+// click event for search button
 $("#submit").click(function() {
-    var cityName = $("#search").val();
-    var url = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`;
+    var searchTerm = $("#search").val();
+    var url = `https://api.openweathermap.org/data/2.5/forecast?q=${searchTerm}&cnt=5&appid=${apiKey}`;
     var historyEl = $("#history");
     if (!historyList) {
         historyList = [];
     }
-    historyList.push({name: cityName});
+    historyList.push({name: searchTerm});
     localStorage.setItem("historyList", JSON.stringify(historyList));
     historyList.forEach(city => {
         historyCityNameEl = $("<h4>").addClass("cityHistoryItem").text(city.name);
@@ -18,12 +18,14 @@ $("#submit").click(function() {
 
     fetch(url).then(function(response) {
         if (response.ok) {
-            response.json();
+            response.json().then(function(data) {
+                console.log(data.list);
+            });
         }
     })
     var cityWeatherSpecsEl = $("<div>");
     cityWeatherSpecsEl.attr("id", "cityWeatherSpecsEl");
-    var cityNameEl = $("<h2>").text(cityName);
+    var cityNameEl = $("<h2>").text(searchTerm);
     cityWeatherSpecsEl.append(cityNameEl);
     var cityTempEl = $("<h3>").text("Temp:");
     cityWeatherSpecsEl.append(cityTempEl);
@@ -35,7 +37,5 @@ $("#submit").click(function() {
     cityWeatherSpecsEl.append(cityUvIndexEl);
     var cityEl = $("#city");
     cityEl.append(cityWeatherSpecsEl);
-})
-
-
+});
 
